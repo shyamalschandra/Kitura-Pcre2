af67e97fdc0b68dc936df3e70a63c35a9b28266a
Daniel Firsht <dfirsht@us.ibm.com>
2015-12-15 17:41:36 -0600

    added common header

---
+++ common.h
@@ -0,0 +1,17 @@
+//
+//  common.h
+//  regex
+//
+//  Created by Samuel Kallner on 11/4/15.
+//  Copyright © 2015 IBM. All rights reserved.
+//
+
+// This include is a hack, to get the define below
+// defined before pcre2.h is included in the umbrella include file
+
+#ifndef common_h
+#define common_h
+
+#define PCRE2_CODE_UNIT_WIDTH 8
+
+#endif /* common_h */
